9556d119046bf8c0dca75facf61f566e4dab5c14
Ev Haus <globexdesigns@gmail.com>
2018-12-08 14:20:01 -0800

    Enable Google Analytics

--- utils/analytics.js
+++ utils/analytics.js
@@ -5,7 +5,7 @@ import ReactGA from 'react-ga';
 export const initGA = () => {
 	const __DEV__ = process.env.NODE_ENV !== 'production';
 
-	ReactGA.initialize('UA-250654-12', {
+	ReactGA.initialize('UA-250654-13', {
 		debug: __DEV__,
 	});
 };
