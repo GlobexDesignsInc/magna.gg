d75c2bbb87c2c92a9351ad3ea40342599915f056
Ev Haus <ev@zenhub.com>
2019-10-17 19:18:39 -0700

    Minor package upgrades and adding beanie to the store

--- pages/store.js
+++ pages/store.js
@@ -15,29 +15,33 @@ export const Store = (): Element<typeof PageTemplate> => (
 			<title>Merchandise Store | Magna eSports</title>
 		</Helmet>
 		<InnerContent title='Merchandise Store' withPadding={true}>
-			<p>Our merchandise store is managed by <a href='https://sbs-streetwear.myshopify.com/'>SBS Streetwear</a> and powered by <a href='https://www.shopify.ca'>Shopify</a>. When you select an item below you will be redirected to a third-party website to complete your order.</p>
+			<p>Our merchandise store is managed by <a href='https://sbstreetwear.com/'>SBS Streetwear</a> and powered by <a href='https://www.shopify.ca'>Shopify</a>. When you select an item below you will be redirected to a third-party website to complete your order.</p>
 
 			<div className={styles.items}>
 				<MerchItem
 					imageUrl='https://cdn.shopify.com/s/files/1/0039/4215/4275/products/mockup-f5ecd655_960x.jpg'
 					name='Magna Esports T-Shirt'
-					url='https://sbs-streetwear.myshopify.com/collections/magna/products/magna-flame-logo-t-shirt' />
+					url='https://sbstreetwear.com/collections/magna/products/magna-flame-logo-t-shirt' />
 				<MerchItem
 					imageUrl='https://cdn.shopify.com/s/files/1/0039/4215/4275/products/mockup-5a133af8_960x.jpg'
 					name='Magna Esports Ignis T-Shirt'
-					url='https://sbs-streetwear.myshopify.com/collections/magna/products/magna-esports-ignis-shirt' />
+					url='https://sbstreetwear.com/collections/magna/products/magna-esports-ignis-shirt' />
 				<MerchItem
 					imageUrl='https://cdn.shopify.com/s/files/1/0039/4215/4275/products/mockup-a18a7f01_960x.jpg'
 					name='Magna Esports Ventus T-Shirt'
-					url='https://sbs-streetwear.myshopify.com/collections/magna/products/magna-esports-ventus-shirt' />
+					url='https://sbstreetwear.com/collections/magna/products/magna-esports-ventus-shirt' />
 				<MerchItem
 					imageUrl='https://cdn.shopify.com/s/files/1/0039/4215/4275/products/mockup-b19e0dab_960x.jpg'
 					name='Magna Esports Terra T-Shirt'
-					url='https://sbs-streetwear.myshopify.com/collections/magna/products/magna-esports-terra-shirt' />
+					url='https://sbstreetwear.com/collections/magna/products/magna-esports-terra-shirt' />
 				<MerchItem
 					imageUrl='https://cdn.shopify.com/s/files/1/0039/4215/4275/products/mockup-c8865c8c_960x.jpg'
 					name='Magna Esports Sweatshirt'
-					url='https://sbs-streetwear.myshopify.com/collections/magna/products/magna-flame-logo-sweatshirt' />
+					url='https://sbstreetwear.com/collections/magna/products/magna-flame-logo-sweatshirt' />
+				<MerchItem
+					imageUrl='https://cdn.shopify.com/s/files/1/0039/4215/4275/products/mockup-90609707_960x.jpg'
+					name='Magna Esports Pom Pom Beanie'
+					url='https://sbstreetwear.com/products/magna-pom-pom-beaine' />
 			</div>
 		</InnerContent>
 	</PageTemplate>
